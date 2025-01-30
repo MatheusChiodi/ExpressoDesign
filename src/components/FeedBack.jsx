44446fa0a27fde686cc7057cbb6b59ab1766fc2d
Matheus Chiodi <matheuschiodi20@gmail.com>
2025-01-30 09:41:48 -0300

    chore(data): create feedbacks data file

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -1,35 +1,7 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-const feedbacks = [
-  {
-    id: 1,
-    name: 'Jane Adams',
-    role: 'Customer',
-    image: './users/user1.png',
-    rating: 4,
-    review:
-      'I dropped by to have a cappuccino and honestly, after 30 days here we did not have high expectations, but this cappuccino was sweet. I would be just as excited! Great place with perfectly normal prices that I recommend!',
-  },
-  {
-    id: 2,
-    name: 'Sam Williams',
-    role: 'Entrepreneur',
-    image: './users/user2.png',
-    rating: 5,
-    review:
-      "I've been ordering beans from you for ten years. The quality of the product is consistently high! I'm grateful for the care that is apparently put into the roasts and the excellent customer service. Your beans give way to the best cups of coffee.",
-  },
-  {
-    id: 3,
-    name: 'Angela Gonzales',
-    role: 'Customer',
-    image: './users/user3.png',
-    rating: 5,
-    review:
-      "I've been ordering beans from you for ten years. The quality of the product is consistently high! I'm grateful for the care that is apparently put into the roasts and the excellent customer service. Your beans give way to the best cups of coffee.",
-  },
-];
+import feedbacks from '../data/feedbacks';
 
 export default function FeedBack() {
   const [currentIndex, setCurrentIndex] = useState(1);
---
+++ src/data/feedbacks.js
@@ -0,0 +1,31 @@
+const feedbacks = [
+  {
+    id: 1,
+    name: 'Jane Adams',
+    role: 'Customer',
+    image: './users/user1.png',
+    rating: 4,
+    review:
+      'I dropped by to have a cappuccino and honestly, after 30 days here we did not have high expectations, but this cappuccino was sweet. I would be just as excited! Great place with perfectly normal prices that I recommend!',
+  },
+  {
+    id: 2,
+    name: 'Sam Williams',
+    role: 'Entrepreneur',
+    image: './users/user2.png',
+    rating: 5,
+    review:
+      "I've been ordering beans from you for ten years. The quality of the product is consistently high! I'm grateful for the care that is apparently put into the roasts and the excellent customer service. Your beans give way to the best cups of coffee.",
+  },
+  {
+    id: 3,
+    name: 'Angela Gonzales',
+    role: 'Customer',
+    image: './users/user3.png',
+    rating: 5,
+    review:
+      "I've been ordering beans from you for ten years. The quality of the product is consistently high! I'm grateful for the care that is apparently put into the roasts and the excellent customer service. Your beans give way to the best cups of coffee.",
+  },
+];
+
+export default feedbacks;
